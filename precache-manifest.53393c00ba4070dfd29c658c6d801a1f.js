1fa060411229c0251a59fc23f159342d1ac8c471
camille-s <camille.seaberry@gmail.com>
2019-12-23 12:41:27 -0500

    Updates

---
+++ precache-manifest.53393c00ba4070dfd29c658c6d801a1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231762f02318a0740edea9b21e21c293",
+    "url": "/index2019/index.html"
+  },
+  {
+    "revision": "ac4a3df5c8c69d95ce78",
+    "url": "/index2019/static/css/2.c297138a.chunk.css"
+  },
+  {
+    "revision": "9efccbcbd3d55a1e02cb",
+    "url": "/index2019/static/css/main.432c01b9.chunk.css"
+  },
+  {
+    "revision": "ac4a3df5c8c69d95ce78",
+    "url": "/index2019/static/js/2.63ae89f1.chunk.js"
+  },
+  {
+    "revision": "0b451a3db72da8e91ffdf2d950b1c9d8",
+    "url": "/index2019/static/js/2.63ae89f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9efccbcbd3d55a1e02cb",
+    "url": "/index2019/static/js/main.3b89ece4.chunk.js"
+  },
+  {
+    "revision": "12d27c3fed2c86d005d1",
+    "url": "/index2019/static/js/runtime-main.365945a4.js"
+  }
+]);
