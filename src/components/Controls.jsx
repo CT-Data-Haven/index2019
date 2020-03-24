c8865ab5de87d4d0dc25fcd9c6868b6aa41d74fa
camille-s <camille.seaberry@gmail.com>
2020-03-24 16:16:16 -0400

    Refactor controls

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,34 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Form, Col, Row } from 'react-bootstrap';
 import { useFormContext } from 'react-hook-form';
 import { cleanIdxLabels, cleanHdrLabels, getComparables } from './utils.js';
 
 import '../styles/Controls.css';
 
-const ScoreMainControls = (props) => {
-  const { register } = useFormContext();
+// controls:
+// region
+// topic
+// question
+// group
+// index
+// secondary
+// age group
+// condition
 
+const ControlHolder = (props) => {
   return (
     <div className='Controls'>
       <Form>
         <Row>
           <Col>
-            <Form.Group controlId='v1Select'>
-              <Form.Label>Index values by group</Form.Label>
-              <Form.Control as='select' name='v1Select' className='custom-select' ref={ register } onChange={ props.onChange }>
-                { props.variables.map((d, i) => (
-                  <option key={ d + 'v1opt' } value={ d }>{ cleanIdxLabels(d) }</option>
-                )) }
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId='regSelect'>
-              <Form.Label>Filter region</Form.Label>
-              <Form.Control as='select' name='regSelect' className='custom-select' ref={ register } onChange={ props.onChange }>
-                { props.regions.map((d, i) => (
-                  <option key={ d + 'regopt' } value={ d }>{ d }</option>
-                )) }
-              </Form.Control>
-            </Form.Group>
+            { props.children }
           </Col>
         </Row>
       </Form>
@@ -36,36 +29,97 @@ const ScoreMainControls = (props) => {
   );
 };
 
-const ScoreCompareControls = (props) => {
+const Ctrl = (props) => {
   const { register } = useFormContext();
-  const yvariables = getComparables(props.variables);
 
   return (
-    <div className='Controls'>
-      <Form>
-        <Row>
-          <Col>
-            <Form.Group controlId='v2Select'>
-              <Form.Label>Compare { cleanHdrLabels(props.v1, false) } to a second index</Form.Label>
-              <Form.Control as='select' name='v2Select' className='custom-select' ref={ register }  onChange={ props.onChange }>
-                { yvariables.map((d, i) => (
-                  <option key={ d + 'v2opt' } value={ d }>{ cleanIdxLabels(d) }</option>
-                )) }
-              </Form.Control>
-            </Form.Group>
-          </Col>
-        </Row>
-      </Form>
-    </div>
+    <Form.Group controlId={ props.name }>
+      <Form.Label>{ props.label || 'Select a ' + props.name.substring(1) }</Form.Label>
+      <Form.Control
+        as='select'
+        name={ props.name }
+        className='custom-select'
+        ref={ register }
+        onChange={ props.onChange }
+      >
+        { props.keys.map((d) => (
+          <option key={ (d.value || d) + props.name } value={ d.value || d }>
+            { d.display || cleanHdrLabels(d) }
+          </option>
+        )) }
+      </Form.Control>
+    </Form.Group>
   );
 };
+
+const NestedCtrl = (props) => {
+  const { register } = useFormContext();
+
+  return (
+    <Form.Group controlId={ props.name }>
+      <Form.Label>{ props.label || 'Select a ' + props.name.substring(1) }</Form.Label>
+      <Form.Control
+        as='select'
+        name={ props.name }
+        className='custom-select'
+        ref={ register }
+        onChange={ props.onChange }
+      >
+        { Object.keys(props.keys).map((d, i) => (
+          <optgroup label={ d } key={ i + 'optgrp' }>
+            { props.keys[d].map((e) => (
+              <option value={ e } key={ e + 'grp' }>{ e }</option>
+            )) }
+          </optgroup>
+        )) }
+      </Form.Control>
+    </Form.Group>
+  );
+};
+
+const SurveyMainControls = (props) => (
+  <React.Fragment>
+    { /* region */ }
+    <Ctrl name='_region' keys={ props.regions } onChange={ props.onChange } />
+    { /* topic */ }
+    <Ctrl name='_topic' keys={ props.topics } onChange={ props.onChange } />
+    { /* question */ }
+    <Ctrl name='_indicator'
+      keys={ props.meta.map((d) => ({ value: d.indicator, display: d.display })) }
+      onChange={ props.onChange }
+      label='Select a survey question'
+    />
+  </React.Fragment>
+);
+
+const GroupControls = (props) => (
+  <NestedCtrl name='_group' keys={ props.groups } onChange={ props.onChange } />
+);
+
+const ScoreMainControls = (props) => (
+  <React.Fragment>
+    { /* region */ }
+    <Ctrl name='_region' keys={ props.regions } onChange={ props.onChange } />
+    { /* score */ }
+    <Ctrl name='_v1' keys={ props.variables } onChange={ props.onChange } label='Select an Index' />
+  </React.Fragment>
+);
+
+const ScoreCompareControls = (props) => (
+  <Ctrl name='_v2'
+    keys={ getComparables(props.variables).map((d) => ({
+      value: d, display: cleanIdxLabels(d)
+    })) }
+    onChange={ props.onChange }
+    label={ `Compare ${ cleanHdrLabels(props.v1, false) } to a second index` }
+  />
+);
 
 const SparkControls = (props) => (
   <div className='Controls SparkControls float-right'>
     <Form.Check
       inline
       custom
-      // default
       checked={ props.checked }
       id='show-spark-bars'
       name='sparkToggle'
@@ -75,99 +129,30 @@ const SparkControls = (props) => (
   </div>
 );
 
-const SurveyTopicFragment = (props) => (
+const RiskMainControls = (props) => (
   <React.Fragment>
-    <Form.Group controlId='topicSelect'>
-      <Form.Label>Select a topic</Form.Label>
-      <Form.Control as='select' name='topicSelect' className='custom-select' ref={ props.register } onChange={ props.onChange }>
-        { props.topics.map((d, i) => (
-          <option key={ d + 'topicopt' } value={ d }>{ cleanHdrLabels(d) }</option>
-        )) }
-      </Form.Control>
-    </Form.Group>
-
-    <Form.Group controlId='qSelect'>
-      <Form.Label>Select survey question</Form.Label>
-      <Form.Control as='select' name='qSelect' className='custom-select' ref={ props.register } onChange={ props.onChange }>
-        { props.topicMeta.map((d, i) => (
-          <option key={ d.indicator + 'qopt' } value={ d.indicator }>{ d.display }</option>
-        )) }
-      </Form.Control>
-    </Form.Group>
+    { /* topic */ }
+    <Ctrl name='_topic' keys={ props.topics } onChange={ props.onChange } />
+    { /* question */ }
+    <Ctrl name='_indicator'
+      keys={ props.indicators.map((d) => ({ value: d.indicator, display: d.display })) }
+      onChange={ props.onChange }
+      label='Select a survey question'
+    />
   </React.Fragment>
 );
 
-const SurveyMainControls = (props) => {
-  const { register } = useFormContext();
+const ChimeMainControls = (props) => (
+  <React.Fragment>
+    { /* condition */ }
+    <Ctrl name='_indicator'
+      keys={ props.indicators.map((d) => ({ value: d.indicator, display: d.display })) }
+      onChange={ props.onChange }
+      label='Select a condition'
+    />
+    { /* age */ }
+    <Ctrl name='_age' keys={ props.ages } onChange={ props.onChange } label='Select an age group' />
+  </React.Fragment>
+);
 
-  return (
-    <div className='Controls'>
-      <Form>
-        <Row>
-          <Col>
-            <Form.Group controlId='regSelect'>
-              <Form.Label>Filter region</Form.Label>
-              <Form.Control as='select' name='regSelect' className='custom-select' ref={ register } onChange={ props.onChange }>
-                { props.regions.map((d, i) => (
-                  <option key={ d + 'regopt' } value={ d }>{ d }</option>
-                )) }
-              </Form.Control>
-            </Form.Group>
-
-            <SurveyTopicFragment { ...props } register={ register } />
-          </Col>
-        </Row>
-      </Form>
-    </div>
-  );
-};
-
-const SurveyProfileControls = (props) => {
-  const { register } = useFormContext();
-  const categories = Object.keys(props.groups);
-
-  return (
-    <div className='Controls'>
-      <Form>
-        <Row>
-          <Col>
-            <Form.Group controlId='grpSelect'>
-              <Form.Label>Select a group</Form.Label>
-              <Form.Control as='select' name='grpSelect' className='custom-select' ref={ register } onChange={ props.onChange }>
-                {
-                  categories.map((d, i) => (
-                    <optgroup label={ d } key={ d + 'optgrp' }>
-                      { props.groups[d].map((e, j) => (
-                        <option value={ e } key={ j + 'grp' }>{ e }</option>
-                      )) }
-                    </optgroup>
-                  ))
-                }
-              </Form.Control>
-            </Form.Group>
-          </Col>
-        </Row>
-      </Form>
-    </div>
-  )
-};
-
-const RiskMainControls = (props) => {
-  const { register } = useFormContext();
-
-  return (
-    <div className='Controls'>
-      <Form>
-        <Row>
-          <Col>
-            <SurveyTopicFragment { ...props } register={ register } />
-          </Col>
-        </Row>
-      </Form>
-    </div>
-  )
-};
-
-
-// export default Controls;
-export { ScoreMainControls, ScoreCompareControls, SurveyMainControls, SurveyProfileControls, SparkControls, RiskMainControls };
+export { ScoreMainControls, ScoreCompareControls, SurveyMainControls, GroupControls, SparkControls, RiskMainControls, ChimeMainControls, ControlHolder };
